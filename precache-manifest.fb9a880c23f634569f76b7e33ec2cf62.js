fdec80a425cc4e1bf739df2de10e271d0b93accb
AppVeyor <ci@appveyor.com>
2020-11-09 13:24:33 +0000

    c1520ec

---
+++ precache-manifest.fb9a880c23f634569f76b7e33ec2cf62.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbd61650c5e7f7af3ed24f767941043",
+    "url": "/R-hw-6-task-1/index.html"
+  },
+  {
+    "revision": "5b4f96e4e1dbee0eed95",
+    "url": "/R-hw-6-task-1/static/css/main.ac1ae2af.chunk.css"
+  },
+  {
+    "revision": "317aba4d74fda077342a",
+    "url": "/R-hw-6-task-1/static/js/2.5b9524f3.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/R-hw-6-task-1/static/js/2.5b9524f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4f96e4e1dbee0eed95",
+    "url": "/R-hw-6-task-1/static/js/main.6b38f98c.chunk.js"
+  },
+  {
+    "revision": "d7486829e4a8e5ffdf2d",
+    "url": "/R-hw-6-task-1/static/js/runtime-main.f3be1454.js"
+  },
+  {
+    "revision": "3a87595845f0e6fed3a625dd6ce7b49d",
+    "url": "/R-hw-6-task-1/static/media/clockFace.3a875958.svg"
+  },
+  {
+    "revision": "e9ad96918a2a655397ec3921e0d9d156",
+    "url": "/R-hw-6-task-1/static/media/hourHand.e9ad9691.svg"
+  },
+  {
+    "revision": "d5db17579470d30ce322482b67db775d",
+    "url": "/R-hw-6-task-1/static/media/minuteHand.d5db1757.svg"
+  },
+  {
+    "revision": "dffcd1d13c3a8e706af7996f60139260",
+    "url": "/R-hw-6-task-1/static/media/secondHand.dffcd1d1.svg"
+  }
+]);
